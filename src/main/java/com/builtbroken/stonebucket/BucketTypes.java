889d2eeb4636d7914d4814fa0edc5acc89f09a51
Robert Seifert <rseifert.phone@gmail.com>
2017-07-27 17:54:24 -0400

    Worked on localizations

--- src/main/java/com/builtbroken/stonebucket/BucketTypes.java
+++ src/main/java/com/builtbroken/stonebucket/BucketTypes.java
@@ -2,6 +2,10 @@ package com.builtbroken.stonebucket;
 
 import com.builtbroken.mc.fluids.FluidModule;
 import com.builtbroken.mc.fluids.bucket.BucketMaterial;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockStone;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
@@ -10,15 +14,30 @@ public enum BucketTypes
 {
     STONE("block@minecraft:stone"),
     SAND_STONE("block@minecraft:sandstone"),
-    END_STONE("block@minecraft:end_stone");
+    END_STONE("block@minecraft:end_stone"),
+    GRANITE("block@minecraft:stone", BlockStone.EnumType.GRANITE.getMetadata()),
+    POLISHED_GRANITE("block@minecraft:stone", BlockStone.EnumType.GRANITE_SMOOTH.getMetadata()),
+    DIORITE("block@minecraft:stone", BlockStone.EnumType.DIORITE.getMetadata()),
+    POLISHED_DIORITE("block@minecraft:stone", BlockStone.EnumType.DIORITE_SMOOTH.getMetadata()),
+    ANDESITE("block@minecraft:stone", BlockStone.EnumType.ANDESITE.getMetadata()),
+    POLISHED_ANDESITE("block@minecraft:stone", BlockStone.EnumType.ANDESITE_SMOOTH.getMetadata()),
+    MOSSY_COBBLE("block@minecraft:end_stone"),
+    PRISMARINE("block@minecraft:prismarine");
 
     public BucketMaterial material;
 
     public String craftingMaterial;
+    public int meta = 0;
 
     BucketTypes(String craftingMaterial)
     {
         this.craftingMaterial = craftingMaterial;
+    }
+
+    BucketTypes(String craftingMaterial, int meta)
+    {
+        this.craftingMaterial = craftingMaterial;
+        this.meta = meta;
     }
 
     public Object getCraftingMaterial()
@@ -28,12 +47,20 @@ public enum BucketTypes
             if (craftingMaterial.startsWith("block@"))
             {
                 String sub = craftingMaterial.substring(6, craftingMaterial.length());
-                return ForgeRegistries.BLOCKS.getValue(new ResourceLocation(sub));
+                Block block = ForgeRegistries.BLOCKS.getValue(new ResourceLocation(sub));
+                if (block != null && block != Blocks.AIR)
+                {
+                    return new ItemStack(block, 1, meta);
+                }
             }
             else if (craftingMaterial.startsWith("item@"))
             {
                 String sub = craftingMaterial.substring(5, craftingMaterial.length());
-                return ForgeRegistries.ITEMS.getValue(new ResourceLocation(sub));
+                Item item = ForgeRegistries.ITEMS.getValue(new ResourceLocation(sub));
+                if (item != null && item != Item.getItemFromBlock(Blocks.AIR))
+                {
+                    return new ItemStack(item, meta);
+                }
             }
             return craftingMaterial;
         }
